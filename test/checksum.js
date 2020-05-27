5577fd485dac24ba3a708b3e1ac8e1d6162b4e6a
Marcel Verpaalen <marcel@verpaalen.com>
2020-05-27 20:12:52 +0200

    append checksum if it was missing

--- test/checksum.js
+++ test/checksum.js
@@ -6,41 +6,31 @@ describe('CheckSum', function () {
 
     var sentence1 = '$GPBOD,045.,T,023.,M,DEST,START';
     it('Checksum should be added and equal *01', function (done) {
-        expect(utils.checksummedSentence(sentence1, true)).to.equal('$GPBOD,045.,T,023.,M,DEST,START*01');
+        expect(utils.appendChecksum(sentence1)).to.equal('$GPBOD,045.,T,023.,M,DEST,START*01');
         done();
     });
 
-    var sentence2 = '$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,';
-    it('Checksum should not be added', function (done) {
-        expect(utils.checksummedSentence(sentence2, false)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,');
+    var sentence2 = '!GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,';
+    it('Checksum should be added and equal *6F', function (done) {
+        expect(utils.appendChecksum(sentence2)).to.equal('!GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6F');
         done();
     });
 
-    it('Checksum should not be added for undefined', function (done) {
-        expect(utils.checksummedSentence(sentence2)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,');
+    var sentence3 = '#GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,';
+    it('Checksum should not be appended', function (done) {
+        expect(utils.appendChecksum(sentence3)).to.equal('#GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,');
         done();
     });
-    var sentence3 = '$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6A';
-    it('Checksum should be recalculated to *6F', function (done) {
-        expect(utils.checksummedSentence(sentence3, true)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6F');
-        done();
-    });
-
+ 
     var sentence4 = '$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6A';
     it('Checksum should not be recalculated to *6F', function (done) {
-        expect(utils.checksummedSentence(sentence4, false)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6A');
+        expect(utils.appendChecksum(sentence4)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6A');
         done();
     });
 
-    var sentence5 = '$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6';
-    it('Garbage checksum should be recalculated to *6F', function (done) {
-        expect(utils.checksummedSentence(sentence5, true)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6F');
-        done();
-    });
-
-    var sentence6 = '$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6';
-    it('Garbage checksum should not be recalculated to *6F', function (done) {
-        expect(utils.checksummedSentence(sentence6, false)).to.equal('$GPGGA,000000.00,5253.164,N,00539.655,E,0,00,99.9,,M,,M,,*6');
+    var sentence5 = '$GPBOD,045.,T,023.,M,DEST*,START*';
+    it('Checksum should not be appended', function (done) {
+        expect(utils.appendChecksum(sentence5)).to.equal('$GPBOD,045.,T,023.,M,DEST*,START*');
         done();
     });
 
