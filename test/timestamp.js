4a933c595d4ae66aba9f117a086b12b44ecc01c8
joabakk <github@heiamoss.com>
2021-02-11 14:55:01 +0100

    fix: assume year is after 2000

--- test/timestamp.js
+++ test/timestamp.js
@@ -3,15 +3,15 @@ var expect  = chai.expect;
 var utils   = require('../index');
 
 describe('Timestamp', function() {
-  var value = utils.timestamp('220104', '171089');
+  var value = utils.timestamp('220104', '171019');
 
   it('Should be a string', function(done) {
     expect(value).to.be.a('string');
     done();
   });
 
-  it('Should equal 1989-10-17T22:01:04.000Z', function(done) {
-    expect(value).to.equal('1989-10-17T22:01:04.000Z');
+  it('Should equal 2019-10-17T22:01:04.000Z', function(done) {
+    expect(value).to.equal('2019-10-17T22:01:04.000Z');
     done();
   });
 });
