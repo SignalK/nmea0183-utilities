62d8420baf5bfb233bf37949e819c8a62a234a28
Jeremy Waters <jaffadog@gmail.com>
2025-10-06 15:08:16 -1000

    style: format project using prettier
    add prettier dev dependency
    use same prettierrc config as signalk-server
    run prettier on whole project

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-'use strict';
-
-(function () {
-
-  var utils = {};
+'use strict'
+;(function () {
+  var utils = {}
 
   utils.RATIOS = {
     // DISTANCE
@@ -33,177 +31,185 @@
     CELCIUS_IN_KELVIN: 273.15,
     // Length
     METER_IN_FEET: 3.2808,
-    METER_IN_FATHOM: 0.5468,
-  };
+    METER_IN_FATHOM: 0.5468
+  }
 
   function checksum(sentencePart) {
-    let check = 0;
+    let check = 0
     for (let i = 1; i < sentencePart.length; i++) {
-      check = check ^ sentencePart.charCodeAt(i);
-    };
-    return check;
+      check = check ^ sentencePart.charCodeAt(i)
+    }
+    return check
   }
 
   exports.valid = function (sentence, validateChecksum) {
-    sentence = String(sentence).trim();
+    sentence = String(sentence).trim()
 
-    if (sentence === "") {
-      return false;
+    if (sentence === '') {
+      return false
     }
 
-    var validateChecksum = typeof validateChecksum === 'undefined' || validateChecksum
+    var validateChecksum =
+      typeof validateChecksum === 'undefined' || validateChecksum
 
-    if ((sentence.charAt(0) == '$' || sentence.charAt(0) == '!') && (validateChecksum == false || sentence.charAt(sentence.length - 3) == '*')) {
+    if (
+      (sentence.charAt(0) == '$' || sentence.charAt(0) == '!') &&
+      (validateChecksum == false || sentence.charAt(sentence.length - 3) == '*')
+    ) {
       if (validateChecksum) {
-        let split = sentence.split('*');
-        return (parseInt(split[1], 16) == checksum(split[0]));
+        let split = sentence.split('*')
+        return parseInt(split[1], 16) == checksum(split[0])
       } else {
         return true
       }
     }
-    return false;
-  };
+    return false
+  }
 
   exports.appendChecksum = function (sentence) {
-    let split = String(sentence).trim().split('*');
+    let split = String(sentence).trim().split('*')
     if (split.length === 1) {
       if (split[0].charAt(0) == '$' || split[0].charAt(0) == '!') {
-        return split[0].concat('*').concat(checksum(split[0]).toString(16).padStart(2, '0').toUpperCase());
+        return split[0]
+          .concat('*')
+          .concat(
+            checksum(split[0]).toString(16).padStart(2, '0').toUpperCase()
+          )
       }
     }
-    return sentence;
-  };
+    return sentence
+  }
 
   exports.source = function (sentence) {
     return {
       type: 'NMEA0183',
       label: 'signalk-parser-nmea0183',
       sentence: sentence || ''
-    };
-  };
+    }
+  }
 
   exports.transform = function (value, inputFormat, outputFormat) {
-    value = exports.float(value);
+    value = exports.float(value)
 
-    inputFormat = inputFormat.toLowerCase();
-    outputFormat = outputFormat.toLowerCase();
+    inputFormat = inputFormat.toLowerCase()
+    outputFormat = outputFormat.toLowerCase()
 
     if (inputFormat === outputFormat) {
-      return value;
+      return value
     }
 
     // KM
     if (inputFormat == 'km') {
-      if (outputFormat == 'nm') return value / utils.RATIOS.NM_IN_KM;
+      if (outputFormat == 'nm') return value / utils.RATIOS.NM_IN_KM
     }
 
     // NM
     if (inputFormat == 'nm') {
-      if (outputFormat == 'km') return value / utils.RATIOS.KM_IN_NM;
-      if (outputFormat == 'm') return value * 1000 / utils.RATIOS.KM_IN_NM;
+      if (outputFormat == 'km') return value / utils.RATIOS.KM_IN_NM
+      if (outputFormat == 'm') return (value * 1000) / utils.RATIOS.KM_IN_NM
     }
 
     // KNOTS
     if (inputFormat == 'knots') {
-      if (outputFormat == 'kph') return value / utils.RATIOS.KPH_IN_KNOTS;
-      if (outputFormat == 'ms') return value / utils.RATIOS.MS_IN_KNOTS;
-      if (outputFormat == 'mph') return value / utils.RATIOS.MPH_IN_KNOTS;
+      if (outputFormat == 'kph') return value / utils.RATIOS.KPH_IN_KNOTS
+      if (outputFormat == 'ms') return value / utils.RATIOS.MS_IN_KNOTS
+      if (outputFormat == 'mph') return value / utils.RATIOS.MPH_IN_KNOTS
     }
 
     // KPH
     if (inputFormat == 'kph') {
-      if (outputFormat == 'knots') return value / utils.RATIOS.KNOTS_IN_KPH;
-      if (outputFormat == 'ms') return value / utils.RATIOS.MS_IN_KPH;
-      if (outputFormat == 'mph') return value / utils.RATIOS.MPH_IN_KPH;
+      if (outputFormat == 'knots') return value / utils.RATIOS.KNOTS_IN_KPH
+      if (outputFormat == 'ms') return value / utils.RATIOS.MS_IN_KPH
+      if (outputFormat == 'mph') return value / utils.RATIOS.MPH_IN_KPH
     }
 
     // MPH
     if (inputFormat == 'mph') {
-      if (outputFormat == 'knots') return value / utils.RATIOS.KNOTS_IN_MPH;
-      if (outputFormat == 'ms') return value / utils.RATIOS.MS_IN_MPH;
-      if (outputFormat == 'kph') return value / utils.RATIOS.KPH_IN_MPH;
+      if (outputFormat == 'knots') return value / utils.RATIOS.KNOTS_IN_MPH
+      if (outputFormat == 'ms') return value / utils.RATIOS.MS_IN_MPH
+      if (outputFormat == 'kph') return value / utils.RATIOS.KPH_IN_MPH
     }
 
     // MS
     if (inputFormat == 'ms') {
-      if (outputFormat == 'knots') return value / utils.RATIOS.KNOTS_IN_MS;
-      if (outputFormat == 'mph') return value / utils.RATIOS.MPH_IN_MS;
-      if (outputFormat == 'kph') return value / utils.RATIOS.KPH_IN_MS;
+      if (outputFormat == 'knots') return value / utils.RATIOS.KNOTS_IN_MS
+      if (outputFormat == 'mph') return value / utils.RATIOS.MPH_IN_MS
+      if (outputFormat == 'kph') return value / utils.RATIOS.KPH_IN_MS
     }
 
     // ANGLES
     if (inputFormat == 'deg') {
-      if (outputFormat == 'rad') return value / utils.RATIOS.RAD_IN_DEG;
+      if (outputFormat == 'rad') return value / utils.RATIOS.RAD_IN_DEG
     }
 
     if (inputFormat == 'rad') {
-      if (outputFormat == 'deg') return value / utils.RATIOS.DEG_IN_RAD;
+      if (outputFormat == 'deg') return value / utils.RATIOS.DEG_IN_RAD
     }
 
     if (inputFormat == 'c') {
-      if (outputFormat == 'k') return value + utils.RATIOS.CELCIUS_IN_KELVIN;
+      if (outputFormat == 'k') return value + utils.RATIOS.CELCIUS_IN_KELVIN
     }
 
     if (inputFormat == 'k') {
-      if (outputFormat == 'c') return value - utils.RATIOS.CELCIUS_IN_KELVIN;
-      if (outputFormat == 'f') return (1.8 * (value - 273)) + 32;
+      if (outputFormat == 'c') return value - utils.RATIOS.CELCIUS_IN_KELVIN
+      if (outputFormat == 'f') return 1.8 * (value - 273) + 32
     }
     // LENGTH
     if (inputFormat == 'ft') {
-      if (outputFormat == 'm') return value / utils.RATIOS.METER_IN_FEET;
+      if (outputFormat == 'm') return value / utils.RATIOS.METER_IN_FEET
     }
 
     if (inputFormat == 'fa') {
-      if (outputFormat == 'm') return value / utils.RATIOS.METER_IN_FATHOM;
+      if (outputFormat == 'm') return value / utils.RATIOS.METER_IN_FATHOM
     }
     // Just return input if input/output formats are not recognised.
-    return value;
-  };
+    return value
+  }
 
   exports.magneticVariaton = function (degrees, pole) {
-    pole = pole.toUpperCase();
-    degrees = this.float(degrees);
+    pole = pole.toUpperCase()
+    degrees = this.float(degrees)
 
-    if (pole == "S" || pole == "W") {
-      degrees *= -1;
+    if (pole == 'S' || pole == 'W') {
+      degrees *= -1
     }
 
-    return degrees;
-  };
+    return degrees
+  }
 
   exports.timestamp = function (time, date) {
     /* TIME (UTC) */
-    var hours, minutes, seconds, year, month, day;
+    var hours, minutes, seconds, year, month, day
 
     if (time) {
-      hours = this.int(time.slice(0, 2), true);
-      minutes = this.int(time.slice(2, 4), true);
-      seconds = this.int(time.slice(4, 6), true);
+      hours = this.int(time.slice(0, 2), true)
+      minutes = this.int(time.slice(2, 4), true)
+      seconds = this.int(time.slice(4, 6), true)
     } else {
-      var dt = new Date();
-      hours = dt.getUTCHours();
-      minutes = dt.getUTCMinutes();
-      seconds = dt.getUTCSeconds();
+      var dt = new Date()
+      hours = dt.getUTCHours()
+      minutes = dt.getUTCMinutes()
+      seconds = dt.getUTCSeconds()
     }
 
     /* DATE (UTC) */
     if (date) {
-      var year, month, day;
-      day = this.int(date.slice(0, 2), true);
-      month = this.int(date.slice(2, 4), true); // this will be a value 1-12
-      year = this.int(date.slice(4, 6), true);
-      year = 2000 + year;
+      var year, month, day
+      day = this.int(date.slice(0, 2), true)
+      month = this.int(date.slice(2, 4), true) // this will be a value 1-12
+      year = this.int(date.slice(4, 6), true)
+      year = 2000 + year
     } else {
-      var dt = new Date();
-      year = dt.getUTCFullYear();
-      month = dt.getUTCMonth() + 1; // getUTCMonth() returns 0-11
-      day = dt.getUTCDate();
+      var dt = new Date()
+      year = dt.getUTCFullYear()
+      month = dt.getUTCMonth() + 1 // getUTCMonth() returns 0-11
+      day = dt.getUTCDate()
     }
 
     /* construct */
-    var d = new Date(Date.UTC(year, (month - 1), day, hours, minutes, seconds)); // month is expected to be 0-11
-    return d.toISOString();
-  };
+    var d = new Date(Date.UTC(year, month - 1, day, hours, minutes, seconds)) // month is expected to be 0-11
+    return d.toISOString()
+  }
 
   exports.coordinate = function (value, pole) {
     // N 5222.3277 should be read as 52°22.3277'
@@ -218,62 +224,61 @@
     // 4°54'34.944'' E -> 4.909706667
     // S & W should be negative.
 
-    pole = pole.toUpperCase();
+    pole = pole.toUpperCase()
 
-    var split = value.split('.');
-    var degrees = this.float(split[0].slice(0, -2));
-    var minsec = this.float(split[0].slice(-2) + '.' + split[1]);
-    var decimal = this.float(degrees + (minsec / 60));
+    var split = value.split('.')
+    var degrees = this.float(split[0].slice(0, -2))
+    var minsec = this.float(split[0].slice(-2) + '.' + split[1])
+    var decimal = this.float(degrees + minsec / 60)
 
-    if (pole == "S" || pole == "W") {
-      decimal *= -1;
+    if (pole == 'S' || pole == 'W') {
+      decimal *= -1
     }
 
-    return exports.float(decimal);
-  };
+    return exports.float(decimal)
+  }
 
   exports.isValidPosition = function (latitude, longitude) {
     if (
-      typeof latitude !== "number" ||
-      typeof longitude !== "number" ||
+      typeof latitude !== 'number' ||
+      typeof longitude !== 'number' ||
       Math.abs(latitude) > 90 ||
       Math.abs(longitude) > 180
     ) {
-      return false;
+      return false
     }
-    return true;
-  };
+    return true
+  }
 
   exports.zero = function (n) {
     if (this.float(n) < 10) {
-      return "0" + n;
+      return '0' + n
     } else {
-      return "" + n;
+      return '' + n
     }
-  };
+  }
 
   exports.int = function (n) {
-    if (("" + n).trim() === '') {
-      return 0;
+    if (('' + n).trim() === '') {
+      return 0
     } else {
-      return parseInt(n, 10);
+      return parseInt(n, 10)
     }
-  };
+  }
 
   exports.integer = function (n) {
-    return exports.int(n);
-  };
+    return exports.int(n)
+  }
 
   exports.float = function (n) {
-    if (("" + n).trim() === '') {
-      return 0.0;
+    if (('' + n).trim() === '') {
+      return 0.0
     } else {
-      return parseFloat(n);
+      return parseFloat(n)
     }
-  };
+  }
 
   exports.double = function (n) {
-    return exports.float(n);
-  };
-
-})();
+    return exports.float(n)
+  }
+})()
